c8400e4f33f5ec6a4435f898db0b5b4a1a242dfa
FabianMosa <fabiancarab@gmail.com>
2025-10-30 11:10:19 -0300

    comentario

--- config/db.js
+++ config/db.js
@@ -7,9 +7,9 @@ export const pool = mysql.createPool({
   user: process.env.DB_USER || 'root',
   password: process.env.DB_PASSWORD || 'bfms1009',
   database: process.env.DB_DATABASE || 'bodetic',
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0
+  //waitForConnections: true,
+  //connectionLimit: 10,
+  //queueLimit: 0
 });
 
 console.log('Conexión a la BD establecida con éxito.');
