84190ba2e4c7860f14137f34d76b9b23c1db7703
FabianMosa <fabiancarab@gmail.com>
2025-11-11 12:27:19 -0300

    update checkbox proveedor

--- controllers/categoria.controller.js
+++ controllers/categoria.controller.js
@@ -1,12 +1,13 @@
 
 import { pool } from '../config/db.js';
 
-// GET (Leer todas las categorías)
+// GET (Obtener todas las categorías para los dropdowns)
 export const getCategorias = async (req, res) => {
   try {
-    const [rows] = await pool.query('SELECT * FROM CATEGORIA ORDER BY nombre_categoria ASC');
+    const [rows] = await pool.query('SELECT PK_id_categoria, nombre_categoria FROM CATEGORIA ORDER BY nombre_categoria ASC');
     res.json(rows);
   } catch (error) {
+    console.error("Error en getCategorias:", error);
     return res.status(500).json({ message: 'Error interno del servidor' });
   }
 };
